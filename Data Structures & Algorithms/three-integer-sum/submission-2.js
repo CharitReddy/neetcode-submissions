f493db528e1fb433142901ead260270f44705f8f
Charit Reddy <41099525+CharitReddy@users.noreply.github.com>
2026-09-23 00:16:23 -0400

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const len = nums.length;
+        const res = [];
+        
+        nums.sort((a,b)=>a-b);
+
+        for(let i=0;i<len;i++){
+            if (i>0 && nums[i]===nums[i-1]) continue;
+            const target = -nums[i];
+
+            let left = i+1;
+            let right = len-1;
+
+            while(left<right){                
+                if(nums[left]+nums[right] === target) {
+                    res.push([nums[i],nums[left],nums[right]]);
+                    left++;
+                    right--;
+                    
+while (left < right && nums[left] === nums[left - 1]) {
+    left++;
+}
+
+while (left < right && nums[right] === nums[right + 1]) {
+    right--;
+}
+                };
+                if(nums[left]+nums[right] > target) right--;
+                if(nums[left]+nums[right] < target) left++;
+            }            
+        }
+
+        return res;
+    }
+}
